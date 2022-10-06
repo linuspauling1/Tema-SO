15c9b5d1352dbff184cb5497663481e6f79d3bc0
linuspauling1 <paulb2233@yahoo.com>
2022-10-06 11:14:07 +0300

    Refactorizare TOTALA

--- tema.c
+++ tema.c
@@ -4,79 +4,106 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <sys/types.h>
+
+#define MAX_LENGTH 256
 
 void prelucrareRezultatePipe();
 
-int main(int argc, char* argv[])
-{
-    if(argc < 2) { //error check
-        fprintf(stderr,"Argumente insuficiente.\n");
+int main(int argc, char* argv[]) {
+    struct stat tampon;
+    if(argc < 2) {
+        fprintf(stderr,"Argumente insuficiente!\n");
         exit(1);
     }
-    struct stat tampon;
-    int stat_error = stat(argv[1], &tampon);
-    if(stat_error < 0) { //error check
-        perror(NULL);
+    if(argc > 2) {
+        fprintf(stderr,"Argumente excesive!\n");
         exit(2);
     }
-    if(!S_ISDIR(tampon.st_mode)) { //error check
-        fprintf(stderr,"Argumentul nu este director.\n");
+    if(stat(argv[1],&tampon) == -1) {
+        perror("Eroare la analiza fisierului: ");
         exit(3);
     }
-    int fd_pipe[2];
-    int err_pipe = pipe(fd_pipe);
-    int in = dup(0);
-    #include "errors/e1.h"
-    int out = dup(1);
-    #include "errors/e2.h"
-    int pid = fork();
-    if(pid == 0) { //error check
-        int close_err = close(fd_pipe[0]);
-        #include "errors/e3.h"
-        char* argumente[4];
-        argumente[0] = "ls";
-        argumente[1] = "-al";
-        argumente[2] = argv[1];
-        argumente[3] = NULL;
-        int dup2_error = dup2(fd_pipe[1],1);
-        #include "errors/e7.h"
-        int execvp_err = execvp(argumente[0],argumente);
-        #include "errors/e11.h"
-        close_err = close(fd_pipe[1]);
-        #include "errors/e4.h"
-        dup2_error = dup2(out,1);
-        #include "errors/e8.h"
-        return 0;
-    }
-    else if(pid > 0) {
-        int close_err = close(fd_pipe[1]);
-        #include "errors/e5.h"
-        int dup2_error = dup2(fd_pipe[0],0);
-        #include "errors/e9.h"
-        
-        prelucrareRezultatePipe();
-
-        int wait_err = wait(NULL);
-        #include "errors/e12.h"
-        close_err = close(fd_pipe[0]);
-        #include "errors/e6.h"
-        dup2_error = dup2(in,0);
-        #include "errors/e10.h"
-    }
-    else if(pid < 0) { //error check
-        perror("Armarea procesului a esuat.");
+    if(!S_ISDIR(tampon.st_mode)) {
+        fprintf(stderr,"Fisierul oferit ca argument nu este director!\n");
         exit(4);
+    }
+    int fd[2];
+    if(pipe(fd) == -1) {
+        perror("Eroare la crearea pipe-ului: ");
+        exit(5);
+    }
+    int pid = fork();
+    if(pid == -1) {
+        perror("Eroare la crearea procesului fiu: ");
+        exit(6);
+    }
+    if(pid == 0) {
+        if(close(fd[0]) == -1) {
+            perror("Eroare la close: ");
+            exit(7);
+        }
+        char * arguments[4];
+        arguments[0] = "ls";
+        arguments[1] = "-al";
+        arguments[2] = argv[1];
+        arguments[3] = NULL;
+        if(dup2(fd[1],STDOUT_FILENO) == -1) {
+            perror("Eroare la dup2: ");
+            exit(8);
+        }
+        if(execvp(arguments[0],arguments) == -1) {
+            perror("Nu a functionat executia...");
+            if(close(fd[1]) == -1){
+                perror("Eroare la close: ");
+                exit(-1);
+            }
+            exit(9);
+        }
+    } else {
+        if(close(fd[1]) == -1) {
+            perror("Eroare la close: ");
+            exit(10);
+        }
+        int tmp = dup(STDIN_FILENO);//pregatire restaurare STDIN
+        if(tmp == -1) {
+            perror("Eroare la dup: ");
+            exit(11);
+        }
+        if(dup2(fd[0],STDIN_FILENO) == -1) {
+            perror("Eroare la dup2: ");
+            exit(12);
+        }
+        prelucrareRezultatePipe();
+        if(close(fd[0]) == -1) {
+            perror("Eroare la close: ");
+            exit(13);
+        }
+        if(dup2(tmp, STDERR_FILENO) == -1) {//restaurarea descriptorului STDIN
+            perror("Eroare la dup2: ");
+            exit(14);
+        }
+        int wstatus;
+        if(wait(&wstatus) == -1) {
+            perror("Eroare la wait: ");
+            exit(15);
+        }
+        if(WIFEXITED(wstatus)) {
+            int exit_status = WEXITSTATUS(wstatus);
+            if(exit_status == 0)
+                printf("Comanda a rulat cu succes!\n");//optional
+            else
+                fprintf(stderr,"Comanda NU a rulat...\n");
+        }
     }
     return 0;
 }
 
 void prelucrareRezultatePipe() {
-    char s[256];
-    while(fgets(s,256,stdin)) {
+    char s[MAX_LENGTH];
+    while(fgets(s,MAX_LENGTH,stdin)) {
         s[strlen(s) - 1] = 0;
         int i;
-        char iesire[7][256];
+        char iesire[7][MAX_LENGTH];
         for(i = 0;i < 7;++i)
             scanf("%s",iesire[i]);
         printf("%s %s %s\n",iesire[6],iesire[4],iesire[2]);
